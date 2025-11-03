33d727d12262b04050339808a2dcd715bb3ad7e9
catorceno <camilacatorceno005@gmail.com>
2025-11-03 16:49:53 -0400

    dll 5

--- database/dll.sql
+++ database/dll.sql
@@ -1,148 +1,155 @@
-CREATE DATABASE TeatroTickets2;
+CREATE DATABASE TeatroTickets5;
 GO
-USE TeatroTickets2;
+USE TeatroTickets5
 GO
 
 CREATE TABLE USERS (
-	userID	   INT			 PRIMARY KEY IDENTITY(1,1),
+	userID	   INT PRIMARY KEY IDENTITY(1,1),
 	email	   NVARCHAR(100) NOT NULL,
 	[password] NVARCHAR(200) NOT NULL,
 	rol		   NVARCHAR(10)  NOT NULL,
-	CONSTRAINT CHK_user_rol CHECK ( rol IN ('theater', 'customer', 'performer'))
+	CONSTRAINT CHK_user_rol CHECK(rol IN ('theater', 'customer', 'performer'))
 );
+CREATE TABLE CUSTOMERS (
+	customerID INT PRIMARY KEY IDENTITY(1,1),
+	userID INT		    NOT NULL,
+	[name] NVARCHAR(50) NOT NULL,
+	phone  NVARCHAR(20) NOT NULL,
+	CONSTRAINT FK_customer_user FOREIGN KEY (userID) REFERENCES USERS(userID)
+);
+CREATE TABLE PERFORMERS (
+	performerID INT PRIMARY KEY IDENTITY(1,1),
+	userID		INT		     NULL,
+	[name]		NVARCHAR(50) NOT NULL,
+	direction	NVARCHAR(50) NULL,
+	contact		NVARCHAR(50) NULL,
+	[state]		NVARCHAR(20) NOT NULL,
+	[type]    NVARCHAR(20) NOT NULL,
+	CONSTRAINT FK_performer_user FOREIGN KEY (userID) REFERENCES USERS(userID),
+	CONSTRAINT CHK_performer_state CHECK([state] IN ('registered', 'not registered')), -- userID NOT NULL = registered
+	CONSTRAINT CHK_performer_type CHECK ([type] IN ('academy', 'company', 'independent'))
+);
+ALTER TABLE PERFORMERS
+ALTER COLUMN [type] NVARCHAR(20) NOT NULL;
 
+/*if is register:
+	- it has its own profile
+	- type of performers:
+		- academy, company: direction
+		- independent
+*/
 CREATE TABLE THEATERS (
-	theaterID	  INT			PRIMARY KEY IDENTITY(1,1),
-	userID		  INT			NOT NULL,
-	[name]		  NVARCHAR(50)  NOT NULL,
-	direction	  NVARCHAR(100) NOT NULL,
-	manager		  NVARCHAR(50)  NOT NULL, -- REVISAR
-	phone		  NVARCHAR(20), -- REVISAR: si habrá más de un número de contacto para el museo, sera el del manager? el contacto del vendedor
-	email		  NVARCHAR(100),
-	capacity INT			NOT NULL,
+	theaterID INT PRIMARY KEY IDENTITY(1,1),
+	userID	  INT		   NOT NULL,
+	[name]	  NVARCHAR(50) NOT NULL,
+	direction NVARCHAR(50) NOT NULL, -- REVISAR COMO GUARDAR ESTO
+	capacity  INT		   NOT NULL,
+	contact	  NVARCHAR(50) NULL,
 	CONSTRAINT FK_theater_user FOREIGN KEY (userID) REFERENCES USERS(userID),
-	CONSTRAINT CHK_theater_capacity CHECK(capacity > 0)
+	CONSTRAINT CHK_theater_capacity CHECK(capacity > 0) -- capacity < ?
 );
 CREATE TABLE SEATING_AREAS (
-	seatingAreaID INT		    PRIMARY KEY IDENTITY(1,1),
-	theaterID	  INT		    NOT NULL,
-	[name]	      NVARCHAR(50)  NOT NULL,
-	capacity	  INT		    NOT NULL, -- capacity <= theaterID.capacity
+	seatingAreaID INT PRIMARY KEY IDENTITY(1,1),
+	theaterID	  INT		   NOT NULL,
+	[name]		  NVARCHAR(50) NOT NULL, -- name.Upper
+	capacity	  INT		   NOT NULL,
 	CONSTRAINT FK_seatingArea_theater FOREIGN KEY (theaterID) REFERENCES THEATERS(theaterID),
-	CONSTRAINT CHK_seatingArea_capacity CHECK(capacity > 0)
+	CONSTRAINT CHK_seatingArea_capacity CHECK(capacity > 0) -- && capacity <= theaterID.capacity 
 );
 CREATE TABLE SEATS (
-	seatID		  INT		   PRIMARY KEY IDENTITY(1,1),
-	seatingAreaID INT		   NOT NULL,
-	[row]		  NVARCHAR(2)  NOT NULL, -- constraint?
-	number		  INT		   NOT NULL,
+	seatID		  INT PRIMARY KEY IDENTITY(1,1),
+	seatingAreaID INT		  NOT NULL,
+	[row]		  NVARCHAR(2) NOT NULL, -- row.Upper
+	number		  INT		  NOT NULL, -- REVISAR QUE TIPO DE DATOS DEBERÍA SER?
 	CONSTRAINT FK_seat_seatingArea FOREIGN KEY (seatingAreaID) REFERENCES SEATING_AREAS(seatingAreaID),
 	CONSTRAINT CHK_seat_number CHECK(number > 0)
 );
 
-CREATE TABLE PERFORMERS (
-	performerID INT			 PRIMARY KEY IDENTITY(1,1),
-	userID		INT,
-	[name]		NVARCHAR(50) NOT NULL,
-	-- contact -- NULL
-	rol			NVARCHAR(10) NOT NULL, -- organizer, performer. Para organizer userID obligatorio
-	CONSTRAINT FK_performer_user FOREIGN KEY (userID) REFERENCES USERS(userID),
-	CONSTRAINT CHK_performer_rol CHECK(rol IN ('organizer', 'performer'))
-);
-CREATE TABLE [EVENTS] ( -- festival
-	eventID		  INT			PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE [EVENTS] (
+	eventID		  INT PRIMARY KEY IDENTITY(1,1),
 	theaterID	  INT			NOT NULL,
-	performerID	  INT, -- organizer -- NULL
-	title		  NVARCHAR(50),	
-	[description] NVARCHAR(200),
-	category	  NVARCHAR(10)  NOT NULL,
+	performerID	  INT			NULL,
+	title		  NVARCHAR(50)  NULL,
+	[description] NVARCHAR(200) NULL,
+	playbillPDF	  NVARCHAR(200) NULL, -- REVISAR como guardar
+	category	  NVARCHAR(20)  NOT NULL,
 	[type]		  NVARCHAR(10)  NOT NULL,
+	[state]		  NVARCHAR(10)  NOT NULL,
 	CONSTRAINT FK_event_theater FOREIGN KEY (theaterID) REFERENCES THEATERS(theaterID),
 	CONSTRAINT FK_event_performer FOREIGN KEY (performerID) REFERENCES PERFORMERS(performerID),
-	CONSTRAINT CHK_event_categoty CHECK(category IN ('music', 'dance', 'theatre')),
-	CONSTRAINT CHK_event_type CHECK([type] IN ('festival', 'show', 'unique play'))
+	CONSTRAINT CHK_event_category CHECK(category IN ('music', 'dance', 'theatre')),
+	CONSTRAINT CHK_event_type CHECK([type] IN ('festival', 'show', 'unique play')),
+	CONSTRAINT CHK_event_state CHECK([state] IN ('draft', 'published', 'completed'))
 );
-/*
-festival --> unique event, different plays, and there are at least 1 different performer of a play
-show --> unique event, different plays but organizer and performer are the same
-unique play --> unique play, organizer and performer are the same
-
-client: events, plays
-theaters and performers: festival, complete show, unique play
-*/
-CREATE TABLE EVENT_PLAYS (
-	eventID INT NOT NULL,
-	playID  INT NOT NULL,
-	CONSTRAINT FK_event_play FOREIGN KEY (eventID) REFERENCES [EVENTS](eventID),
-	CONSTRAINT FK_play_event FOREIGN KEY (playID) REFERENCES PLAYS(playID)
-);
-CREATE TABLE PLAYS ( -- concert
-	playID		  INT			PRIMARY KEY IDENTITY(1,9),
-	performerID	  INT			NOT NULL,-- performer -- NULL
+CREATE TABLE PLAYS (
+	playID		  INT PRIMARY KEY IDENTITY(1,1),
+	eventID		  INT			NOT NULL,
+	performerID	  INT		    NOT NULL,
 	title		  NVARCHAR(50)  NOT NULL,
-	[description] NVARCHAR(75),
-	playbillPDF	  NVARCHAR(200),
-	duration	  INT			NOT NULL
-);
-CREATE TABLE PLAYS_PERFORMANCES (
-	playID			
-	performanceID
+	[description] NVARCHAR(200) NULL,
+	duration	  INT			NOT NULL,
+	CONSTRAINT FK_play_event FOREIGN KEY (eventID) REFERENCES [EVENTS](eventID),
+	CONSTRAINT FK_play_performer FOREIGN KEY (performerID) REFERENCES PERFORMERS(performerID)
 );
 CREATE TABLE PERFORMANCES (
-	performanceID
-	[datetime]
-	[state]
-	CONSTRAINT CHK_event_datetime CHECK([datetime] > GETDATE()),
-	CONSTRAINT CHK_event_duration CHECK(duration > 0),
-	CONSTRAINT CHK_event_state CHECK([state] IN ('presale', 'onsale', 'soldout', 'completed', 'canceled'))
+	performanceID INT PRIMARY KEY IDENTITY(1,1),
+	[datetime]	  DATETIME NOT NULL,
+	[state]		  NVARCHAR(10) NOT NULL,
+	CONSTRAINT CHK_performance_datetime CHECK([datetime] > GETDATE()),
+	CONSTRAINT CHK_performance_state CHECK([state] IN ('presale', 'onsale', 'soldout', 'completed', 'canceled'))
+);
+CREATE TABLE PLAYS_PERFORMANCE (
+	playID		  INT NOT NULL,
+	performanceID INT NOT NULL,
+	CONSTRAINT FK_playPerformance_play FOREIGN KEY (playID) REFERENCES PLAYS(playID),
+	CONSTRAINT FK_playPerformance_performance FOREIGN KEY (performanceID) REFERENCES PERFORMANCES(performanceID)
 );
 
 CREATE TABLE PRICE_ZONES (
-	priceZoneID
-	performanceID
-	[name]
-	pricePresale
-	priceOnsale
+	priceZoneID	  INT PRIMARY KEY IDENTITY(1,1),
+	performanceID INT		    NOT NULL,
+	[name]		  NVARCHAR(50)  NOT NULL,
+	pricePresale  DECIMAL(10,2) NULL,
+	price         DECIMAL(10,2) NOT NULL,
+	CONSTRAINT CHK_priceZone_pricePresale CHECK(pricePresale >= 0),
+	CONSTRAINT CHK_priceZone_price CHECK(price >= 0)
 );
 CREATE TABLE PRICE_ZONE_SEATS (
-	priceZoneSeatID
-	priceZoneID
-	seatID
-	[state]
-	CONSTRAINT FK_priceZoneSeat_eventPriceZone FOREIGN KEY (priceZoneID) REFERENCES EVENT_PRICE_ZONES(priceZoneID),
+	priceZoneSeatID INT PRIMARY KEY IDENTITY(1,1),
+	priceZoneID		INT			 NOT NULL,
+	seatID			INT			 NOT NULL,
+	[state]			NVARCHAR(10) NOT NULL CONSTRAINT DF_priceZoneSeat_state DEFAULT 'available',
+	CONSTRAINT FK_priceZoneSeat_priceZone FOREIGN KEY (priceZoneID) REFERENCES PRICE_ZONES(priceZoneID),
 	CONSTRAINT FK_priceZoneSeat_seat FOREIGN KEY (seatID) REFERENCES SEATS(seatID),
 	CONSTRAINT CHK_priceZoneSeat_state CHECK([state] IN ('available', 'occupied', 'disabled', 'completed'))
 );
 
-
--- cada performance debe tener su propio mapa de asientos
-
 CREATE TABLE RESERVATIONS (
-	reservationID INT			PRIMARY KEY IDENTITY(1,1),
-	userID		  INT			NULL, -- must be role = customer
-	eventID		  INT			NOT NULL,
-	[date]		  DATETIME		NOT NULL, -- date <= eventID.datetime, default = GETDATE()
-	total		  DECIMAL(10,2) NOT NULL,
-	CONSTRAINT FK_reservation_user FOREIGN KEY (userID) REFERENCES USERS(userID),
-	CONSTRAINT FK_reservation_event FOREIGN KEY (eventID) REFERENCES [EVENTS](eventID)
+	reservationID INT PRIMARY KEY IDENTITY(1,1),
+	customerID	  INT			NULL,
+	eventID		  INT			NOT NULL, -- debería ser performanceID
+	total		  DECIMAL(10,2) NOT NULL, -- NULL
+	[date]		  DATETIME		NOT NULL CONSTRAINT DF_reservation_date DEFAULT GETDATE(), -- < performance.datetime
+	CONSTRAINT FK_reservation_customer FOREIGN KEY (customerID) REFERENCES CUSTOMERS(customerID),
+	CONSTRAINT FK_reservation_event FOREIGN KEY (eventID) REFERENCES [EVENTS](eventID),
+	CONSTRAINT CHK_reservation_total CHECK (total >= 0)
 );
-
 CREATE TABLE TICKETS (
-	ticketID	  INT			PRIMARY KEY IDENTITY(1,1),
-	reservationID INT			NOT NULL,
-	seatID		  INT			NOT NULL,
-	price		  DECIMAL(10,2) NOT NULL,
-	CONSTRAINT FK_tickets_reservation FOREIGN KEY (reservationID) REFERENCES RESERVATIONS(reservationID),
-	CONSTRAINT FK_tickets_seat FOREIGN KEY (seatID) REFERENCES SEATS(seatID),
-	CONSTRAINT CHK_tickets_price CHECK(price >= 0)
+	ticketID		INT PRIMARY KEY IDENTITY(1,1),
+	reservationID   INT NOT NULL,
+	priceZoneSeatID INT NOT NULL,
+	price			INT NOT NULL,
+	CONSTRAINT FK_ticket_reservation FOREIGN KEY (reservationID) REFERENCES RESERVATIONS(reservationID),
+	CONSTRAINT FK_ticket_priceZoneSeat FOREIGN KEY (priceZoneSeatID) REFERENCES PRICE_ZONE_SEATS(priceZoneSeatID),
+	CONSTRAINT CHK_ticket_price CHECK(price >= 0)
 );
-
 CREATE TABLE PAYMENTS (
-	paymentID	  INT			PRIMARY KEY IDENTITY(1,1),
-	reservationID INT			NOT NULL,
-	method		  NVARCHAR(10)	NOT NULL,
+	paymentID	  INT PRIMARY KEY IDENTITY(1,1),
+	reservationID INT		    NOT NULL,
+	method		  NVARCHAR(20)  NOT NULL,
 	amount		  DECIMAL(10,2) NOT NULL,
-	[date]		  DATETIME		NOT NULL, -- date <= reservationID.date, default = GETDATE()
+	[date]		  DATETIME		NOT NULL CONSTRAINT DF_payment_date DEFAULT GETDATE(), -- <= reservationID.date
 	CONSTRAINT FK_payment_reservation FOREIGN KEY (reservationID) REFERENCES RESERVATIONS(reservationID),
+	CONSTRAINT CHK_payment_method CHECK(method IN ('qr', 'tarjeta', 'efectivo')),
 	CONSTRAINT CHK_payment_amount CHECK(amount > 0)
 );
--- database/funcionalidades.sql
+++ database/funcionalidades.sql
@@ -1,40 +1,184 @@
-USE TeatroTickets2;
+USE TeatroTickets5
 GO
 
--- AGREGAR UN TEATRO
--- crear usuario
-INSERT INTO USERS(email, [password], rol)
-VALUES ('theater@gmail', '12345', 'theater');
+---------------------------- REGISTRAR UN TEATRO
+-- crear cuenta
+DECLARE @email1 NVARCHAR(100) = 'theater1@gmail.com'
+DECLARE @password1 NVARCHAR(200) = 'theater1123'
+INSERT INTO USERS(email, password, rol)
+VALUES (@email1, @password1, 'theater');
 GO
 -- formulario con los datos del teatro
-INSERT INTO THEATERS(userID, [name], direction, phone, email, capacity)
-VALUES (1, 'theater1', 'my direction', NULL, NULL, 10);
+DECLARE @userID1 INT = 1
+DECLARE @name1 NVARCHAR(50) = 'Teatro Municipal 1'
+DECLARE @direction1 NVARCHAR(50) = 'doble via km.7'
+DECLARE @capacity1 INT = 10
+DECLARE @contact NVARCHAR(50) = 'encargado: 77999914, ventas: 76400014'
+INSERT INTO THEATERS(userID, [name], direction, capacity, contact)
+VALUES (@userID1, @name1, @direction1, @capacity1, @contact);
 GO
-
 -- mapa del teatro
 INSERT INTO SEATING_AREAS(theaterID, [name], capacity)
-VALUES (1, 'platea', 6), (1, 'anfiteatro', 4);
+VALUES
+	(1, 'platea', 5),
+	(1, 'anfiteatro', 5);
 GO
-INSERT INTO BUTACAS(seatingAreaID, [row], number)
+INSERT INTO SEATS(seatingAreaID, [row], number)
 VALUES
 	(1, 'A', 1),
 	(1, 'A', 3),
 	(1, 'A', 5),
-	(1, 'B', 2),
+	(1, 'B', 3),
 	(1, 'B', 4),
-	(1, 'B', 6),
-	(1, 'A', 1),
-	(1, 'A', 3),
-	(1, 'B', 2),
-	(1, 'B', 4);
+	(2, 'A', 1),
+	(2, 'A', 3),
+	(2, 'A', 5),
+	(2, 'B', 2),
+	(2, 'B', 4);
 GO
 
--- AGREGAR UN EVENTO, desde el teatro
--- crear evento
-INSERT INTO [EVENTS](theaterID, title, [description], category, [datetime], duration, [state])
+---------------------------- REGISTRAR UN EVENTO COMO TEATRO
+	-- categoria: festival
+-- crear usuario
+INSERT INTO USERS(email, password, rol)
+VALUES ('fases@gmail.com', 'fases123', 'performer');
+GO
+-- formulario con los datos del performer
+INSERT INTO PERFORMERS(userID, name, direction, contact, state, type)
+VALUES
+	(1, 'fases', 'villa', '76869337', 'registered', 'academy'),
+	(NULL, 'cuerpos libres', 'primer anillo', NULL, 'not registered', 'academy'),
+	(NULL, 'FIDC', 'tercer anillo', '76468337', 'not registered', 'academy'),
+	(NULL, 'arte y canto', 'tercer anillo', '76468337', 'not registered', 'academy'),
+	(NULL, 'Maria Juarez, Andrés ayala y Lucio Loayza', NULL, NULL, 'not registered', 'independent');
+GO
+INSERT INTO [EVENTS](theaterID, performerID, title, description, playbillPDF, category, type, state)
+VALUES (1, 10, 'Festival Internacional de Danza Contemporánea', 'festival', NULL, 'dance', 'festival', 'published');
+GO
+INSERT INTO PLAYS(eventID, performerID, title, description, duration)
 VALUES 
-	(1, 'some event', 'some description', 'music', GETDATE(), 180, 'presale'),
-	(1, 'some event', 'some description', 'music', GETDATE(), 180, 'presale');
+	(1, 10, 'sempiterna', 'description1', 60),
+	(1, 11, 'vuelta', 'description2', 60),
+	(1, 12, 'acero', 'description3', 60),
+	(1, 13, 'acostumbrarse pero puede cambiar', 'description4', 60),
+	(1, 14, 'camila mia', 'description5', 60);
+GO
+INSERT INTO PERFORMANCES(datetime, state)
+VALUES
+	('2025-12-1 19:00:00', 'onsale'),
+	('2025-12-2 19:00:00', 'onsale'),
+	('2025-12-3 19:00:00', 'onsale'),
+	('2025-12-4 19:00:00', 'onsale'),
+	('2025-12-5 19:00:00', 'onsale');
+GO
+INSERT INTO PLAYS_PERFORMANCE(playID, performanceID)
+VALUES
+	(1, 1),
+	(2, 2),
+	(3, 3),
+	(4, 4),
+	(5, 5);
 GO
 
--- 
+-- crear evento - categoria: show
+INSERT INTO [EVENTS]()
+VALUES ();
+GO
+INSERT INTO PLAYS()
+VALUES ();
+GO
+INSERT INTO PERFORMANCES()
+VALUES ();
+GO
+-- crear evento - categoria: unique play
+INSERT INTO [EVENTS]()
+VALUES ();
+GO
+INSERT INTO PLAYS()
+VALUES ();
+GO
+INSERT INTO PERFORMANCES()
+VALUES ();
+GO
+
+---------------------------- DETERMINAR PRECIOS
+select * from PERFORMANCES
+select * from PRICE_ZONES
+INSERT INTO PRICE_ZONES(performanceID, name, price)
+VALUES
+	(1, 'platea', 0),
+	(2, 'platea', 0),
+	(3, 'platea', 0),
+	(4, 'platea', 0),
+	(5, 'platea', 35);
+GO
+INSERT INTO PRICE_ZONE_SEATS(priceZoneID, seatID, state)
+VALUES
+	(1, 1, 'available'),
+	(1, 2, 'available'),
+	(1, 3, 'available'),
+	(1, 4, 'available'),
+	(1, 5, 'available'),
+	(1, 6, 'available'),
+	(1, 7, 'available'),
+	(1, 8, 'available'),
+	(1, 9, 'available'),
+	(1, 10, 'available'),
+	(2, 1, 'available'),
+	(2, 2, 'available'),
+	(2, 3, 'available'),
+	(2, 4, 'available'),
+	(2, 5, 'available'),
+	(2, 6, 'available'),
+	(2, 7, 'available'),
+	(2, 8, 'available'),
+	(2, 9, 'available'),
+	(2, 10, 'available'),
+	(3, 1, 'available'),
+	(3, 2, 'available'),
+	(3, 3, 'available'),
+	(3, 4, 'available'),
+	(3, 5, 'available'),
+	(3, 6, 'available'),
+	(3, 7, 'available'),
+	(3, 8, 'available'),
+	(3, 9, 'available'),
+	(3, 10, 'available'),
+	(4, 1, 'available'),
+	(4, 2, 'available'),
+	(4, 3, 'available'),
+	(4, 4, 'available'),
+	(4, 5, 'available'),
+	(4, 6, 'available'),
+	(4, 7, 'available'),
+	(4, 8, 'available'),
+	(4, 9, 'available'),
+	(4, 10, 'available'),
+	(5, 1, 'available'),
+	(5, 2, 'available'),
+	(5, 3, 'available'),
+	(5, 4, 'available'),
+	(5, 5, 'available'),
+	(5, 6, 'available'),
+	(5, 7, 'available'),
+	(5, 8, 'available'),
+	(5, 9, 'available'),
+	(5, 10, 'available');
+GO
+
+---------------------------- COMPRAR TICKETS
+INSERT INTO RESERVATIONS(eventID, total)
+VALUES (1, 35);
+GO
+INSERT INTO TICKETS(reservationID, priceZoneSeatID, price)
+VALUES
+	(1, 1, 0),
+	(1, 11, 0),
+	(1, 21, 0),
+	(1, 31, 0),
+	(1, 41, 35),
+	(1, 2, 0);
+GO
+INSERT INTO PAYMENTS(reservationID, method, amount)
+VALUES (1, 'qr', 35);
+GO
